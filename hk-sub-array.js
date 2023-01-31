6d780179992cad3860b21aca5c6b636f4b7730f7
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-01 00:54:01 +0600

    HRank sub-array prob solved

--- hk-sub-array.js
+++ hk-sub-array.js
@@ -1,17 +1,43 @@
-let square = [1,2,1,4,5,2];
-var ways = 0 ;
-var sum = 0;
-var j = 0;
-var index = 0 ;
-var m = 0;
-for (i = 0 ; i< square.length ; i++){
-    sum = 0;
-    index = i ;
-    for( j = 0; j < m; j++){
-        sum += square[index++];
-        console.log(sum);
-    }
-    
-    console.log(sum);
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
 
+function birthday(s, d, m) {
+    // Write your code here
+    var i = 0;
+    var j = 0;
+    var index = 0;
+    var len = s.length;
+    var result = 0;
+    var sum = 0;
+
+    // for one element arrow condition
+    if(s.length==1 && s[0] == d){
+        return 1;
+    }
+
+
+    // looping the main array
+    for(i = 0; i< len - (m-1); i++){
+        //m-1 here becaue if we loop throug full length after the last number then it will add sum+NaN-> means it wont get a number
+        sum = 0 ;
+        index = i ;
+        // Creating sub array of m length
+        for ( j = 0 ; j < m ; j++){
+            sum += s[index++];
+        }
+
+        if(sum == d){
+            result++;
+        }
+    }
+
+    return result;
 }
+
