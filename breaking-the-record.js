9e0ae972121eb6fda80b5ba2808b2c061add830f
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-01 14:44:27 +0600

    breaking record solved

--- breaking-the-record.js
+++ breaking-the-record.js
@@ -1,22 +1,28 @@
+//breaking the record
+// The function is expected to return an INTEGER_ARRAY.
+//  * The function accepts INTEGER_ARRAY scores as parameter.
+
+//following fuction accepts scores array as input
+// this fuction will return best and worst in aray
+
 function breakingRecords(scores) {
-    // Write your code here
-    var max = 0;
-    var min = 0;
-    var high = 0;
-    var low = 0;
-    for(let i = 0; i< scores.length ; i++){
-        if(scores[i]> max){
-            max = scores[i];
-            high++;
+    var bestScore = scores[0];
+    var best = 0;    
+    var worstScore = scores[0];
+    var worst = 0;
+    var i = 0;
+    var len = scores.length;
+    for(var i = 0 ; i < len ; i++){
+        if(scores[i]> bestScore){
+            bestScore = scores[i];
+            best++;
             
         }
-        if(scores[i] < min){
-            min = scores[i];
-            low++;
+        else if (scores[i] < worstScore){
+            worstScore = scores[i];
+            worst++;
             
         }
     }
-    
-    return [high, low];
+    return [best, worst];
 }
-
