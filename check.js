e05a74bbb5b53ea3852aeef993f8761daea77540
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-04 23:54:45 +0600

    2 other string prb fixed

---
+++ check.js
@@ -0,0 +1,9 @@
+function reverseString(mystr){
+    for(let i = mystr.length-1; i >=0 ; i--){
+        console.log(mystr[i]);
+    }
+
+}
+
+let myName = "I am sorry";
+console.log(reverseString(myName));
--- reverse-string.js
+++ reverse-string.js
@@ -1,9 +1,31 @@
-function reverseString(mystr){
-    for(let i = mystr.length-1; i >=0 ; i--){
-        console.log(mystr[i]);
-    }
+// reverse by character
 
-}
+// function reverseString(mystr){
+//     for(let i = mystr.length-1; i >=0 ; i--){
+//         console.log(mystr[i]);
+//     }
+
+// }
 
 let text = "Dr. Kushal is a good person";
-let result = reverseString(text);
+// let result = reverseString(text);
+
+ // declaring blank array
+    var items = [];
+// reverse by word
+
+function reverseWords(mystr){
+    const words = mystr.split(' ');
+    // console.log(typeof(words));
+    for(let i = words.length-1; i>= 0 ; i--){
+        // element are each words getting whilelooping
+        let element = words[i];
+        // we are pushing elements in empty item array
+        items.push(element);
+    }
+    //  finally joining the array elemnt with* to show as sentence
+    // here white space can be used
+    console.log(items.join(' '));
+    
+}
+ reverseWords(text);
