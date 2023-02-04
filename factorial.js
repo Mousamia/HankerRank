a20cf7d5618b2f230b2742e639d9a0591bc347fd
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-05 00:36:56 +0600

    factiral

---
+++ factorial.js
@@ -0,0 +1,17 @@
+function factorial(n){
+    var result = 1;
+    
+    for(let i = 1; i <= number; i++){
+        
+        // result = 1*1(i), 1 = 1*2(i), 2 = 2*3(i), 6*4(i),
+        result =  result*i;
+        console.log("result + i", result, i);
+    }
+    console.log(" i am u looking for",result);
+}
+
+
+
+var number = 6;
+factorial(number);
+factorial(7);
