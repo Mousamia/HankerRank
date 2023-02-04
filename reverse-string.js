049dc8a3d8a97fab0370d521af8d994c91564212
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-04 22:57:08 +0600

    reverse by char

---
+++ reverse-string.js
@@ -0,0 +1,9 @@
+function reverseString(mystr){
+    for(let i = mystr.length-1; i >=0 ; i--){
+        console.log(mystr[i]);
+    }
+
+}
+
+let text = "Dr. Kushal is a good person";
+let result = reverseString(text);
